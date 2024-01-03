429863fcd9b3c44d83bd00c42fb55d5c830db6a7
sunmin <zysyw@163.com>
2024-01-03 17:36:54 +0800

    遗留问题：元件的能量损耗计算

--- opendss_calculation/run_opendss.py
+++ opendss_calculation/run_opendss.py
@@ -1,5 +1,6 @@
 import opendssdirect as dss
 from flask import Blueprint, jsonify, render_template
+import json
 
 run_opendss_bp = Blueprint('run_opendss_bp', __name__)
 
@@ -33,10 +34,62 @@ def get_line_currents():
 
     return jsonify(currents_data)
 
-@run_opendss_bp.route('/first-meter')
+@run_opendss_bp.route('/show_first-meter')
 def get_opendss_meters():
     meter_names = dss.Meters.RegisterNames()
     meter_values = dss.Meters.RegisterValues()
     registers = zip(meter_names, meter_values)
     
     return render_template('show_meter_registers.html', registers=registers,)
+
+@run_opendss_bp.route('/circut-losses')
+def get_circut_losses():
+    # 收集 EnergyMeter 数据
+    dss.Meters.First()
+    meter_data = {}
+    json_data = 'Null'
+    while True:
+        name = dss.Meters.Name()
+        registers = dss.Meters.RegisterNames()
+        values = dss.Meters.RegisterValues()
+        meter_data[name] = dict(zip(registers, values))
+        if not dss.Meters.Next() > 0:
+            break
+
+    # 收集变压器损耗数据
+    dss.Transformers.First()
+    transformer_losses = {}
+    while True:
+        name = dss.Transformers.Name()
+        print(name)
+        print(dss.Transformers.LossesByType())
+        losses = {
+            "TotalLoss": dss.Transformers.LossesByType()[0]/1000,  # 总损耗,改变单位kVA
+            "LoadLoss": dss.Transformers.LossesByType()[2]/1000,  # 负载损耗
+            "NoLoadLoss": dss.Transformers.LossesByType()[4]/1000  # 空载损耗
+        }
+        transformer_losses[name] = losses
+        if not dss.Transformers.Next() > 0:
+            break
+
+    # 收集线路损耗数据
+    dss.Lines.First()
+    line_losses = {}
+    while True:
+        name = dss.Lines.Name()
+        # 获取损耗（返回的是一个包含有功和无功损耗的元组）
+        losses = dss.CktElement.Losses()[0].real
+        line_losses[name] = losses
+        if not dss.Lines.Next() > 0:
+            break
+
+    # 整合所有数据并转换为 JSON
+    all_data = {
+        "EnergyMeters": meter_data,
+        "TransformerLosses": transformer_losses,
+        "LineLosses": line_losses
+    }
+    json_data = json.dumps(all_data)
+
+    # 输出 JSON 数据或进行后续处理
+    return jsonify(json_data)
